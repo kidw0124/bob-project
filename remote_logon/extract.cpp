ebe9f94bdc2e7535b297683da232726f6f1c2abb
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-12-05 21:56:35 +0900

    Update extract.cpp

--- remote_logon/extract.cpp
+++ remote_logon/extract.cpp
@@ -1,24 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class AuditLogRow{
-    private:
-        string time;
-vector<string> row;
-public:
-    AuditLogRow(vector<string> row){this->row = row;}
+class AuditLogRow {
+ private:
+  string time;
+  vector<string> row;
 
+ public:
+  AuditLogRow(vector<string> row) { this->row = row; }
 };
-int main(int argc, char *argv[])
-{
-    // if(argc != 2)
-    // {
-    //     cout << "Usage: sudo ./extract" << " <filename>" << endl;
-    //     return 1;
-    // }
-    // ifstream fin(argv[1]);
-    string s="";
-    for(int i=0;i<s.size();i++)
-    {
-        cout<<(int)s[i]<<" ";
-    }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout << "Usage: sudo ./extract"
+         << " <filename> [-l <last filename>]" << endl;
+    return -1;
+  }
+  ifstream fin(argv[1]);
 }
