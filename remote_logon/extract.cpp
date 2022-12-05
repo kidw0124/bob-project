02c9fb3febb8d1544cbc3341c14d1a166ab4c9ba
kidw0124 <kidw0124@naver.com>
2022-12-05 21:05:54 +0900

    modify

---
+++ remote_logon/extract.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class AuditLogRow{
+    private:
+        string time;
+vector<string> row;
+public:
+    AuditLogRow(vector<string> row){this->row = row;}
+
+};
+int main(int argc, char *argv[])
+{
+    // if(argc != 2)
+    // {
+    //     cout << "Usage: sudo ./extract" << " <filename>" << endl;
+    //     return 1;
+    // }
+    // ifstream fin(argv[1]);
+    string s="";
+    for(int i=0;i<s.size();i++)
+    {
+        cout<<(int)s[i]<<" ";
+    }
+}
