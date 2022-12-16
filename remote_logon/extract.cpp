97dae59438e2a2428c6b6be4f0e98fb9982dc1ac
kidw0124 <kidw0124@naver.com>
2022-12-16 19:19:50 +0900

    file input

--- remote_logon/extract.cpp
+++ remote_logon/extract.cpp
@@ -9,10 +9,17 @@ class AuditLogRow {
   AuditLogRow(vector<string> row) { this->row = row; }
 };
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
+  if (argc < 2) {
     cout << "Usage: sudo ./extract"
          << " <filename> [-l <last filename>]" << endl;
     return -1;
   }
   ifstream fin(argv[1]);
+  ifstream fin2;
+  if (argc > 2 && strcmp(argv[2], "-l") == 0) {
+    fin2.open(argv[3]);
+  } else {
+    system("sudo last -F > last.txt");
+    fin2.open("last.txt");
+  }
 }
